ed2fafb731f5aa3e4a620dd72f014d4247361698
Karol Moroz <karol@moroz.dev>
2022-09-18 16:03:35 +0200

    Install elixir_latex

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Invoicer.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:gettext] ++ Mix.compilers(),
+      compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
@@ -46,7 +46,8 @@ defmodule Invoicer.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:elixir_latex, path: "../elixir_latex"}
     ]
   end
 
