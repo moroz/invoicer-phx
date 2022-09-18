d1cbd8a561731cd340751488001688f9ebae24ad
Karol Moroz <karol@moroz.dev>
2022-09-19 00:39:17 +0200

    Use data from invoice struct in template

--- lib/invoicer_web/views/invoice_view.ex
+++ lib/invoicer_web/views/invoice_view.ex
@@ -1,6 +1,7 @@
 defmodule InvoicerWeb.InvoiceView do
   use InvoicerWeb, :view
   import InvoicerWeb.TranslationHelpers
+  alias Invoicer.LineItems.LineItem
 
   defmacro table_header(content) do
     ["\\thead{", content, "}"]
