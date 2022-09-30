8b350636561dbdb8e57c0b9f0aa0e151bfb0bd1b
Karol Moroz <k.j.moroz@gmail.com>
2022-10-01 00:36:00 +0200

    Handle invalid cookie, update deps

--- lib/invoicer/users.ex
+++ lib/invoicer/users.ex
@@ -12,6 +12,8 @@ defmodule Invoicer.Users do
     Repo.all(User)
   end
 
+  def get_user(id), do: Repo.get(User, id)
+
   def get_user!(id), do: Repo.get!(User, id)
 
   def create_user(attrs \\ %{}) do
