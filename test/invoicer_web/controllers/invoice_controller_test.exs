66c067d95cdbe364d885c9ee37d32a231f874b40
Karol Moroz <k.j.moroz@gmail.com>
2022-10-05 23:58:01 +0200

    Add more tests for invoice controller

--- test/invoicer_web/controllers/invoice_controller_test.exs
+++ test/invoicer_web/controllers/invoice_controller_test.exs
@@ -1,6 +1,7 @@
 defmodule InvoicerWeb.InvoiceControllerTest do
   use InvoicerWeb.ConnCase
   import Mock
+  alias InvoicerWeb.InvoiceDocument
 
   setup do
     user = insert(:user)
@@ -16,7 +17,20 @@ defmodule InvoicerWeb.InvoiceControllerTest do
   describe "GET show" do
     test "correctly renders file to PDF", ~M{conn, invoice} do
       conn = get(conn, Routes.invoice_path(conn, :show, invoice))
-      assert get_resp_header(conn, "content-type") == ["application/pd"]
+      [content_type] = get_resp_header(conn, "content-type")
+      assert content_type =~ "application/pdf"
+      ["inline"] = get_resp_header(conn, "content-disposition")
+      "%PDF" <> _ = conn.resp_body
+    end
+
+    test "with download=true passed in query string, sets content-disposition: attachment",
+         ~M{conn, invoice} do
+      with_mock(InvoiceDocument, [:passthrough], generate: fn _ -> {:ok, "%PDFtest"} end) do
+        conn = get(conn, Routes.invoice_path(conn, :show, invoice, download: true))
+        [content_type] = get_resp_header(conn, "content-type")
+        assert content_type =~ "application/pdf"
+        ["attachment; filename=" <> _] = get_resp_header(conn, "content-disposition")
+      end
     end
   end
 end
